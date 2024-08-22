09daadae3c4d6b357947707626dbdeab5ef35587
aqeelsalsoft <aqeel.shahid@salsoft.net>
2024-08-22 16:01:08 -0700

    updated nuxt.config.ts file

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -20,10 +20,20 @@ export default defineNuxtConfig({
   // CSS Files Declarations
   css: ["~/assets/css/main.scss"],
 
+  // postcss: {
+  //   plugins: {
+  //     tailwindcss: {},
+  //     autoprefixer: {},
+  //   },
+  // },
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
+      cssnano:
+        process.env.NODE_ENV === 'production'
+          ? { preset: ['default', { discardComments: { removeAll: true } }] }
+          : false, // disable cssnano when not in production
     },
   },
 
