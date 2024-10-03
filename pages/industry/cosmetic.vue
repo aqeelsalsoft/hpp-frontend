b4d310ef0a4c32359320f202959a7f28d62537a0
aqeelsalsoft <aqeel.shahid@salsoft.net>
2024-10-02 21:43:35 -0700

    testing laravel api response on industry page

--- pages/industry/cosmetic.vue
+++ pages/industry/cosmetic.vue
@@ -70,6 +70,36 @@ const backgroundStyles = computed(() => {
     return { backgroundImage: `url('${imgUrl}')` }
 })
 
+const items = [{
+    defaultOpen: true,
+    label: 'What are your minimum order quantities (MOQ)?',
+    description: 'Our minimum order quantity (MOQ) changes based on the distinct product line and customization needs. For more information about the MOQ for the product you\'re inquisitive about, please reach out to our specialists. We seek to accommodate orders of all measures to meet our customers\' various needs, so don\'t hesitate.'
+}, {
+    label: 'Do you offer design assistance?',
+    description: 'Yes, our professional designers can assist you in creating the ideal design for your custom packaging. We function closely with our clients to guarantee your packaging aligns with your brand values and product needs. Also, it is free of cost.'
+}, {
+    label: 'Can I try out samples before purchasing?',
+    description: 'Absolutely! Our sample kit is curated to showcase our top-selling products, allowing you to experience their exceptional quality firsthand. You\'ll get the chance to see and feel the materials up close. Additionally, we offer a convenient swatch book for your reference.'
+}, {
+    label: 'Where to buy boxes?',
+    description: 'You can buy custom boxes in all styles, sizes, shapes, and colors from Half Price Packaging. We offer an all-inclusive range of packaging solutions to meet your needs - primary to secondary.'
+}, {
+    label: 'What types of custom boxes do you offer?',
+    description: 'We deliver a huge collection of bespoke boxes, including corrugated boxes, rigid boxes, gift boxes, folding cartons, mylar bags, mailer boxes, and many other boxes tailored to your precise needs.'
+}, {
+    label: 'What types of packaging materials are available for custom boxes?',
+    description: 'We bring an extensive collection of materials in various thicknesses, including but not limited to eco-friendly Kraft, heavy-duty corrugated, lightweight paperboard and cardboard, bux board, and rigid stock. To keep the uniqueness alive, we also offer metallic stock. For more information, contact our material experts.'
+}, {
+    label: 'How can I get in touch with the packaging experts to discuss material options?',
+    description: 'At Half Price Packaging, you can reach out to our representative through email and call. Also, you can fill the online form and we’ll get back to you with all the answers to your queries.'
+}, {
+    label: 'What is the duration of your production time?',
+    description: 'The production timeline varies from 8-10 business days, depending on the order quantity. After checkout, you will receive an estimated "in hands" date for the boxes shopping cart.'
+}, {
+    label: 'What types of packaging products are available for ordering at Half Price Packaging?',
+    description: 'At Half Price Packaging, we offer an extensive range of packaging products that cater to diverse needs and industries. Whether you\'re looking for custom boxes, CBD boxes, mailer boxes, retail packaging, or product packaging for various items, we have you covered. Our selection includes eco-friendly and sustainable options, such as biodegradable and recyclable materials, making us a go-to source for businesses of all sizes. We also make custom boxes for small businesses.'
+}]
+
 </script>
 
 <template>
@@ -1288,10 +1318,10 @@ const backgroundStyles = computed(() => {
                     <div class="faq__wrapper basis-1/2 pl-[20px]">
                         <h2 class="font-headings text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl mb-[30px]">
                             Frequently Asked <span class="text-[#ef4b5f]">Questions</span></h2>
-                        <UAccordion :items="data.category?.faq">
+                        <UAccordion :items="items">
                             <template #default="{ item, index, open }">
                                 <UButton color="gray" variant="soft" size="xl" class="mb-[10px]">
-                                    <span class="truncate font-headings font-semibold">{{ item.question }}</span>
+                                    <span class="truncate font-headings font-semibold">{{ item.label }}</span>
                                     <template #trailing>
                                         <UIcon name="i-heroicons-chevron-right-20-solid"
                                             class="w-5 h-5 ms-auto transform transition-transform duration-200"
@@ -1302,7 +1332,7 @@ const backgroundStyles = computed(() => {
                             <template #item="{ item }">
                                 <div class="px-[15px]">
                                     <p class="font-description text-[14px] leading-[22px] text-gray-600 mb-[15px]">
-                                        {{ item.answer }}
+                                        {{ item.description }}
                                     </p>
                                 </div>
                             </template>
